eda07b856a57d9bd05361e85a5103eaa79663270
RileyWong26 <rileywongwong@gmail.com>
2025-03-23 12:27:15 -0400

    Backend for user's profile

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const request = require('request');
+const cors = require('cors')
 
 const port = 5000
 
@@ -11,7 +12,8 @@ var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
 var app = express();
 
-var access_token;
+var access_token_player;
+var access_token_profile;
 
 var generateRandomString = function(length) {
     var text = '';
@@ -23,6 +25,11 @@ var generateRandomString = function(length) {
     return text;
 };
 
+// ------ USE CORS -----
+app.use(cors());
+
+
+// ---- AUTHENTICATE LOGIN --------
 app.get('/auth/login', (req, res)=>{
     var scope = "streaming \
     user-read-email\
@@ -34,7 +41,7 @@ app.get('/auth/login', (req, res)=>{
         response_type: "code",
         client_id: spotify_client_id,
         scope: scope,
-        redirect_uri : 'http://127.0.0.1:3000/auth/callback',
+        redirect_uri : 'http://127.0.0.1:5000/auth/callback',
         state: state,
     })
 
@@ -42,17 +49,19 @@ app.get('/auth/login', (req, res)=>{
 
 })
 
+
+// ----- LOGGING IN, GETTING THE USER TOKEN AND PUSHING TO AN API -------------
 app.get('/auth/callback', (req, res) => {
     var code = req.query.code;
     
     var auth_opts = {
         url: "https://accounts.spotify.com/api/token",
         method: "POST",
-        // form: {
-        //     code: code,
-        //     redirect_uri:"http://127.0.0.1:3000/auth/callback",
-        //     grant_type: 'authorization_code'
-        // },
+        form: {
+            code: code,
+            redirect_uri:"http://127.0.0.1:5000/auth/callback",
+            grant_type: 'authorization_code'
+        },
         headers: {
             'Authorization':'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
             'Content-Type': 'application/x-www-form-urlencoded'
@@ -64,21 +73,82 @@ app.get('/auth/callback', (req, res) => {
     request.post(
         auth_opts, 
         function(error, response, body){
-            console.log(error, response.statusCode, body);
             if(!error && response.statusCode === 200){
-                access_token = body.access_token;
-                res.redirect('http://127.0.0.1:3000')
+                access_token_player = body.access_token;
+                res.redirect('http://127.0.0.1:5000/auth/login/user')
         }
     });
 });
 
+
+// --- AUTHENTICATE THE USER -----
+const code_verifier = generateRandomString(64)
+app.get('/auth/login/user', async (req, res)=>{
+    
+    const base64 = async(input) =>{
+        const data = new TextEncoder().encode(input);
+        const digest = await crypto.subtle.digest('SHA-256', data)
+        return btoa(String.fromCharCode.apply(null,[...new Uint8Array(digest)]))
+            .replace(/=+$/, '')
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_');
+    }
+    
+    const codechallenge = await base64(code_verifier)
+    const state = generateRandomString(16)
+    const scope = 'user-read-private user-read-email user-read-playback-state user-modify-playback-state'
+
+    var auth_query_params = new URLSearchParams({
+        client_id: spotify_client_id,
+        response_type: 'code',
+        redirect_uri: 'http://127.0.0.1:5000/auth/callback/user',
+        scope: scope,
+        code_challenge_method : 'S256',
+        code_challenge : codechallenge,
+        state: state
+    })
+
+    res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_params.toString());
+
+})
+
+// --------- CALLBACK FOR USER PROFILE -------
+app.get('/auth/callback/user', (req,res) =>{
+    var code = req.query.code;
+    
+    // ----- ACCESS TOKEN FOR USER'S PROFILE ------------
+    fetch('https://accounts.spotify.com/api/token', {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            client_id: spotify_client_id,
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: 'http://127.0.0.1:5000/auth/callback/user',
+            code_verifier: code_verifier
+        })
+    })
+    .then(response=>response.json())
+    .then(data => access_token_profile = data.access_token)
+    .catch(error => console.log('error',error))
+    res.redirect('http://127.0.0.1:3000')
+})
+
+
+
+// ---- API FOR TOKEN ------
 app.get('/auth/token', (req,res) =>{
     res.json(
         {
-            access_token: access_token
+            access_token_player: access_token_player,
+            access_token_profile: access_token_profile
         }
     )
 })
+
+
 
 
 app.listen(port, () => {
