2f52ed6fa3959695edd233dec51a99d7a34bab9b
RileyWong26 <rileywongwong@gmail.com>
2025-03-27 22:48:57 -0400

    Editing login screen

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from "react";
+import "./Login.css"
 
 
 function Login () {
     
     return (
-        <div>
-            LOGIN!
-            <a href="http://127.0.0.1:5000/auth/login">
+        <div className="Login">
+            <h1 className="Title">LOGIN</h1>
+            <a href="http://127.0.0.1:5000/auth/login" className="LoginButton">
                 <button >Login using spotify <span>Premium required</span></button>
             </a>
         </div>
