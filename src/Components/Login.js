96c51e18a458e9e8379f6857a15f42122c7ca41b
RileyWong26 <rileywongwong@gmail.com>
2025-03-28 12:18:51 -0400

    Login screen

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,7 +9,7 @@ function Login () {
             <h1 className="Title">YOUR MUSIC PLAYER</h1>
             <h1 className="Title2">LOGIN</h1>
             <a href="http://127.0.0.1:5000/auth/login" className="LoginLink">
-                <button className="LoginButton">Connect Spotify</button>
+                <button className="LoginButton">Connect to Spotify</button>
             </a>
         </div>
     )
